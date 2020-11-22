ac714fe338325c061b0be88d7f97c7688b9f1c67
mmcolo <mcolo.miftahou@gmail.com>
2020-11-22 15:52:45 +0100

    update server.js for routing

--- server.js
+++ server.js
@@ -5,5 +5,8 @@ const PORT = process.env.PORT || 5000;
 
 var app_path = '/dist/clamer'
 
-app.use('/', express.static(path.join(__dirname, app_path)))
- .listen(PORT,()=>console.log(`Listen on ${PORT}`));
+app.use(express.static(__dirname+app_path));
+app.get('/*', (req, res)=>{
+    res.sendFile(path.join(__dirname+app_path+'/index.html'));
+});
+app.listen(PORT,()=>console.log(`Listen on ${PORT}`));
